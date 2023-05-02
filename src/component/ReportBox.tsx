79f4c35b0bf1a8e8b1e901197f9ead1bd6097b5c
Shrijan <shrijanb85@gmail.com>
2023-05-02 15:22:33 +0545

    added modal

--- src/component/ReportBox.tsx
+++ src/component/ReportBox.tsx
@@ -1,8 +1,5 @@
 import { ReactNode } from "react";
-import { Link } from "react-router-dom";
-
 import "../styles/style.css";
-
 import ApiData from "../types/ApiData.type";
 
 interface ICardProps {
@@ -20,31 +17,37 @@ const ReportBox = ({
 }: ICardProps) => {
   return (
     <div className="outerContainer">
-      <Link to={`${item.name}`} className="linkBox" target="_self ">
-        <div className="reportBox">
-          <div className="locationBox">
-            <div className="location">{apiData?.name}</div>
-            <div className="date">{dateBuilder}</div>
+      <div className="reportBox">
+        <div className="locationBox">
+          <div className="location">{apiData?.name}</div>
+          <div className="date">{dateBuilder}</div>
+        </div>
+        <div className="weatherBox">
+          <div className="weatherIcon">
+            {apiData?.weather?.map((item, index) => (
+              <img
+                className="logoIcon"
+                key={index}
+                src={`https://openweathermap.org/img/wn/${item.icon}.png`}
+              ></img>
+            ))}
           </div>
-          <div className="weatherBox">
-            <div className="weatherIcon">
-              {apiData?.weather?.map((item, index) => (
-                <img
-                  className="logoIcon"
-                  key={index}
-                  src={`https://openweathermap.org/img/wn/${item.icon}.png`}
-                ></img>
-              ))}
-            </div>
-            <div className="temp">{Math.round(apiData?.main?.temp ?? 0)}°C</div>
-            <div className="weather">
-              {apiData?.weather?.map((item, index) => (
-                <p key={index}>{item?.description}</p>
-              ))}
-            </div>
+          <div className="temp">{Math.round(apiData?.main?.temp ?? 0)}°C</div>
+          <div className="weather">
+            {apiData?.weather?.map((item, index) => (
+              <p key={index}>{item?.description}</p>
+            ))}
           </div>
         </div>
-      </Link>
+      </div>
+      <style>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+          crossOrigin="anonymous"
+        />
+      </style>
       <div className="deleteBox">
         <button
           type="button"
@@ -53,7 +56,7 @@ const ReportBox = ({
         >
           ❌
         </button>
-      </div>
+      </div>{" "}
     </div>
   );
 };
